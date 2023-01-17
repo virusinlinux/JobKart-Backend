46bb40faa296f0b665c0521cf74531905b5ccc42
Yousuf Ejaz Ahmad <ejazahmadyousuf2@gmail.com>
2023-01-18 04:16:55 +0530

    added new routes to get all the candidates

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,20 @@
 let express = require("express"),
-  router = express.Router(),
-  methodOverride = require("method-override"),
-  UserController = require("../controllers/user"),
-  UserService = require("../services/user"),
-  UserManager = require("../managers/user");
+	router = express.Router(),
+	methodOverride = require("method-override"),
+	UserController = require("../controllers/user"),
+	UserService = require("../services/user"),
+	UserManager = require("../managers/user");
 
 const userManager = new UserManager(),
-  userService = new UserService(userManager),
-  userController = new UserController(userService);
+	userService = new UserService(userManager),
+	userController = new UserController(userService);
 
 router.use(methodOverride("_method"));
 
 router.post("/register", (req, res) => userController.createUser(req, res));
+
+router.get("/getAllUsers", (req, res) => {
+	return userController.getAllUsers(req, res);
+});
 
 module.exports = router;
